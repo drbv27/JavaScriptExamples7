2c7633c464f1ae4cb9c4b800fe28a6d0856573a5
Diego Bonilla <drbv27@gmail.com>
2021-05-12 10:13:04 -0500

    First Commit

---
+++ assets/app.js
@@ -0,0 +1,29 @@
+/*Ejercicios Basicos JS Nivel 7
+    21.Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado.Ejemplo: mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+    22.Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array.Ejemplo: miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+    23.Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares.Ejemplo: miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}
+    */
+
+//Soluciones:
+
+//--------------------------------------------------------------------------------------------
+
+//E21S1.Solucion Simple:
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 21:")
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//E22S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 22:")
+
+
+//--------------------------------------------------------------------------------------------
+
+//E23S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 23:")
+
